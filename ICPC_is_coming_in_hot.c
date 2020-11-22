03e6d901039ac5b416ac98a26cc5270238f73fb3
FahimFBA <fahimbinamin@gmail.com>
2020-11-22 22:10:34 +0600

    couldn't solve ICPC yet

---
+++ ICPC_is_coming_in_hot.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    int digit[100000];
+    scanf("%d" , &digit);
+    int i, l, max=0, ans=0;
+    l=strlent(digit);
+    for (i=0; i<10; i++)
+    {
+        
+    }
+}
